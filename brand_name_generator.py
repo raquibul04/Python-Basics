825318807f473d5f9842ef83bec0be75947e8d35
rmowla <raquibul04@gmail.com>
2025-12-07 13:53:22 -0500

    Initial commit

---
+++ brand_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcomne to the Brand Name Generator")
+city = input("What's the name of the city you grew up in?\n ")
+pet = input("What's your pet's name?\n")
+
+print("Your Brand name could be " + city + " " + pet)
