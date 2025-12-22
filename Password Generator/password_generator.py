b4bc787b21aedca30af24116eb7c591119a5f10e
rmowla <raquibul04@gmail.com>
2025-12-22 11:55:48 -0500

    Python Password Generator

---
+++ Password Generator/password_generator.py
@@ -0,0 +1,20 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+ 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+password = random.sample(letters, nr_letters) + random.sample(numbers, nr_numbers) + random.sample(symbols, nr_symbols)
+
+random.shuffle(password)
+final_pass = ''
+
+for pass_word in password:
+    final_pass += pass_word
+
+print(f"Here is your password: {final_pass}")
