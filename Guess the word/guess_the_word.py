6241d3a35ca920b80528c9a8ded11e7ae2ae4878
rmowla <raquibul04@gmail.com>
2025-12-27 13:22:16 -0500

    completed the second phase

--- Guess the word/guess_the_word.py
+++ Guess the word/guess_the_word.py
@@ -2,15 +2,19 @@ import random
 from random import sample
 
 countries = ["United States", "Canada", "United Kingdom", "Germany", "Bangladesh", "Japan", "Australia"]
-
+placeholder = ""
 chosen_word = random.choice(countries).lower()
 print(chosen_word)
+for i in range(len(chosen_word)):
+    placeholder += '_'
+
+print(placeholder)
+display = ""
 guess = input("Take a guess. Give me a letter from the Alphabet\n").lower()
 for letter in chosen_word:
     if letter == guess:
-        print("Right")
+        display += guess
     else:
-        print("Wrong")
+        display += "_"
 
-placeholder = ' _ ' * len(chosen_word)
-print(placeholder)
+print(display)
