dcfc5b5856db978add5a5cb1580088cad50f4179
rmowla <raquibul04@gmail.com>
2026-01-04 23:07:21 -0500

    Started the calculator project_01

--- Calculator Project/calculator.py
+++ Calculator Project/calculator.py
@@ -1,1 +1,12 @@
 #I am about to start on a new project. It will be on building a calculator
+def my_function(a):
+    if a < 40:
+        return
+        print("Terrible")
+    if a < 80:
+        return "Pass"
+    else:
+        return "Great"
+print(my_function(25))
+
+#practice test
