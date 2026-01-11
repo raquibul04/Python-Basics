01324706af9ec086bd89d8340ee0ae0555c363c9
rmowla <raquibul04@gmail.com>
2026-01-10 22:06:39 -0500

    Started the calculator project03

--- Calculator Project/calculator.py
+++ Calculator Project/calculator.py
@@ -11,22 +11,21 @@ def division(a,b):
     return a/b
 sum1 = 0
 while cont_calc:
-
     first_num = int(input("What's the first number?: "))
     operation = input("Pick an operation: ")
     second_num = int(input("What's the next number?: "))
-
+    first_num += sum1
     if operation == '+':
-        sum1 += summation(first_num, second_num)
+        sum1 = summation(first_num, second_num)
     elif operation == '-':
-        sum1 += subtraction(first_num,second_num)
+        sum1 = subtraction(first_num,second_num)
     elif operation == '*':
-        sum1 += multiplication(first_num, second_num)
+        sum1 = multiplication(first_num, second_num)
     elif operation == '/':
-        sum1 += division(first_num,second_num)
+        sum1 = division(first_num,second_num)
     else:
         print("Invalid character")
-    continue_calculation = input(f"Type 'y' to continue calculating with {sum1}, or type 'n' to start a new calculation:")
+    continue_calculation = input(f"Type 'y' to continue calculating with {sum1}, or type 'n' to start a new calculation:").lower()
     if continue_calculation == 'n':
         cont_calc = False
 
