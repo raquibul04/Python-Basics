410b0b444b612d3e6518c5622d24e5ad284208bd
rmowla <raquibul04@gmail.com>
2026-01-10 19:48:50 -0500

    Started the calculator project02

--- Calculator Project/calculator.py
+++ Calculator Project/calculator.py
@@ -1,13 +1,33 @@
 #I am about to start on a new project. It will be on building a calculator
-def my_function(a):
-    if a < 40:
-        return
-        print("Terrible")
-    if a < 80:
-        return "Pass"
-    else:
-        return "Great"
-print(my_function(25))
+cont_calc = True
 
-#practice test_01
-####just another line
+def summation(a,b):
+    return a+b
+def subtraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    return a/b
+sum1 = 0
+while cont_calc:
+
+    first_num = int(input("What's the first number?: "))
+    operation = input("Pick an operation: ")
+    second_num = int(input("What's the next number?: "))
+
+    if operation == '+':
+        sum1 += summation(first_num, second_num)
+    elif operation == '-':
+        sum1 += subtraction(first_num,second_num)
+    elif operation == '*':
+        sum1 += multiplication(first_num, second_num)
+    elif operation == '/':
+        sum1 += division(first_num,second_num)
+    else:
+        print("Invalid character")
+    continue_calculation = input(f"Type 'y' to continue calculating with {sum1}, or type 'n' to start a new calculation:")
+    if continue_calculation == 'n':
+        cont_calc = False
+
+    print(sum1)
